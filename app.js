916061ba2c14dba90ba0666e076b439974517c5d
sakazoo <qq674gzd@gmail.com>
2019-04-26 17:26:49 +0900

    add event

--- app.js
+++ app.js
@@ -20,6 +20,11 @@ var template = new Vue({
       { name: "React", creator: "Facebook", stars: 85623 },
       { name: "Angular", creator: "Google", stars: 31976 }
     ]
+  },
+  methods: {
+    toggle: function(tab) {
+      this.currentTab = tab;
+    }
   }
 });
 
@@ -40,5 +45,51 @@ var binding = new Vue({
     message: "",
     description: "This is textarea.",
     framework: "vue"
+  }
+});
+
+var event = new Vue({
+  el: "#event",
+  data: {
+    rating: 3,
+    comment: "",
+    error: "",
+    submitted: false,
+    previewUrl: ""
+  },
+  methods: {
+    onSubmit: function() {
+      // Web APIをたたく処理など
+
+      // dataの内容はthisからアクセスできる
+      this.error = "";
+      if (this.comment === "") {
+        this.error = "コメントは必須です。";
+        return false;
+      }
+
+      // フォームの内容を表示
+      this.submitted = true;
+    },
+    clearForm: function() {
+      this.submitted = false;
+      this.rating = 3;
+      this.comment = "";
+    },
+    onFileChange: function(event) {
+      const file = event.target.files[0];
+      if (!file) {
+        return false;
+      }
+      if (!file.type.match("image.*")) {
+        return false;
+      }
+      const reader = new FileReader();
+      const that = this;
+      reader.onload = function(e) {
+        that.previewUrl = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    }
   }
 });
