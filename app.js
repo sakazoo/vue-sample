8f5df73e7f50dc789783ec34e79dc1718dbad54a
sakazoo <qq674gzd@gmail.com>
2019-04-26 15:48:13 +0900

    add template sample

--- app.js
+++ app.js
@@ -4,3 +4,21 @@ var app = new Vue({
     message: "Hello world"
   }
 });
+
+var template = new Vue({
+  el: "#template",
+  data: {
+    messages: {
+      count: 1
+    },
+    user: {
+      gender: "male"
+    },
+    currentTab: 1,
+    frameworks: [
+      { name: "Vue", creator: "Evan You", stars: 79760 },
+      { name: "React", creator: "Facebook", stars: 85623 },
+      { name: "Angular", creator: "Google", stars: 31976 }
+    ]
+  }
+});
