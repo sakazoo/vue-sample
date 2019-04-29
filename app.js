67c40180af9794054bc815d17a2ec3db5187c4d9
sakazoo <qq674gzd@gmail.com>
2019-04-29 19:40:35 +0900

    add watch

--- app.js
+++ app.js
@@ -158,3 +158,30 @@ var computed = new Vue({
     }
   }
 });
+
+var watch = new Vue({
+  el: "#watch",
+  data: {
+    email: "",
+    username: "",
+    error: {}
+  },
+  watch: {
+    email: function(value) {
+      if (value === "") {
+        this.error.email = "メールアドレスは必須です。";
+      } else {
+        this.error.email = "";
+      }
+    },
+    username: function(value) {
+      if (value === "") {
+        this.error.username = "ユーザー名は必須です。";
+      } else if (!/[a-zA-Z0-9]+/.test(value)) {
+        this.error.username = "ユーザー名は半角英数のみです。";
+      } else {
+        this.error.username = "";
+      }
+    }
+  }
+});
